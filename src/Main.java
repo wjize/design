3b41e60794da29d861b77939fac5eb3efb09262d
wangjize <y98d225@163.com>
2025-06-13 15:39:35 +0800

    # modify the Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ import static headFirst04.absFactory.MainTest.absFactoryPatten;
 import static headFirst04.factory.MainTest.factoryPattern;
 import static headFirst04.simpleFactory.MainTest.simpleFactoryPattern;
 import static headFirst05.MainTest.singletonPattern;
-import static headFirst06.MainTest.commandPattern;
+import static headFirst06.simple.MainTest.simpleCommandPattern;
+import static headFirst06.remote.MainTest.commandPattern;
 import static headFirst07.adapt.MainTest.adaptPattern;
 import static headFirst07.facade.MainTest.facadePattern;
 import static headFirst08.MainTest.templatePattern;
@@ -26,6 +27,7 @@ public class Main {
         // factoryPattern();
         // absFactoryPatten();
         // singletonPattern();
+        // simpleCommandPattern();
         // commandPattern();
         // adaptPattern();
         // facadePattern();
