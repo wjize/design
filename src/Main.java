edef2631a68f7bd2e97269ee6199d4ea3bdd4aeb
wangjize <y98d225@163.com>
2025-06-16 10:27:31 +0800

    # modify the Main.cpp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import static headFirst01.MainTest.strategyPattern;
+import static headFirst02.MainTest.observerPattern;
+import static headFirst03.MainTest.decorativePattern;
+import static headFirst04.absFactory.MainTest.absFactoryPatten;
+import static headFirst04.factory.MainTest.factoryPattern;
+import static headFirst04.simpleFactory.MainTest.simpleFactoryPattern;
+import static headFirst05.MainTest.singletonPattern;
+import static headFirst06.remote.MainTest.commandPattern;
+import static headFirst06.simple.MainTest.simpleCommandPattern;
+import static headFirst07.adapt.MainTest.adaptPattern;
+import static headFirst07.facade.MainTest.facadePattern;
+import static headFirst08.MainTest.templatePattern;
+import static headFirst09.composite.menu.MainTest.compositePattern;
 import static headFirst09.composite.menuIterator.MainTest.compositeWithIteratorPattern;
-
+import static headFirst09.iterator.MainTest.iteratorPattern;
 
 /**
 *     Created by wangjize on 2025/6/4.
@@ -8,20 +21,20 @@ import static headFirst09.composite.menuIterator.MainTest.compositeWithIteratorP
 public class Main {
     // 每个方法对应一种设计模式
     public static void main(String[] args) {
-//         strategyPattern();
-//         observerPattern();
-//         decorativePattern();
-//         simpleFactoryPattern();
-//         factoryPattern();
-//         absFactoryPatten();
-//         singletonPattern();
-//         simpleCommandPattern();
-//         commandPattern();
-//         adaptPattern();
-//         facadePattern();
-//         templatePattern();
-//         compositePattern();
+         strategyPattern();
+         observerPattern();
+         decorativePattern();
+         simpleFactoryPattern();
+         factoryPattern();
+         absFactoryPatten();
+         singletonPattern();
+         simpleCommandPattern();
+         commandPattern();
+         adaptPattern();
+         facadePattern();
+         templatePattern();
+         compositePattern();
          compositeWithIteratorPattern();
-//         iteratorPattern();
+         iteratorPattern();
     }
 }
